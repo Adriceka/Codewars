85eb36ee2fb003a120334aa4ee3d776ad7f34ef8
adriceka <adrigonzalezgonzalez007@gmail.com>
2026-03-01 12:19:59 +0100

    feat: Añadir kata solucionado
    Añadir kata Pete, the baker solucionado

---
+++ ejercicios/Pete, the baker.py
@@ -0,0 +1,24 @@
+def cakes(recipe, available):
+    cantidades = []
+    
+    for ingrediente in recipe:
+        if ingrediente not in available:
+            return 0
+        posibles = available[ingrediente] // recipe[ingrediente]
+        cantidades.append(posibles)
+
+    minimo = cantidades[0]
+    for num in cantidades:
+        if num < minimo:
+            minimo = num
+
+    return minimo
+
+if __name__ == '__main__':
+    recipe = {"flour": 500, "sugar": 200, "eggs": 1}
+    available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+    assert cakes(recipe, available) == 2
+
+    recipe = {"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}
+    available = {"sugar": 500, "flour": 2000, "milk": 2000}
+    assert cakes(recipe, available) == 0
